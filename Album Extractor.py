c9bc260e1f8051af773bb27ae3b48d5f23d212b4
Avry Luy <avryluy@mailbox.org>
2023-01-17 21:58:22 -0600

    imported modules and wrote sudo-code

---
+++ Album Extractor.py
@@ -0,0 +1,44 @@
+import time as time
+import os as os
+import shutil as sht
+
+#Declare Downloads path
+
+#Declare music library path
+
+#Check for zip files. Determine how many there are.
+
+#Store zip type in a variable
+
+#store zip name in a variable
+
+#splice zip name to get Artist, store in variable
+
+#splice zip name to get album name, store in variable
+
+#For each zip, Unpack the zipfile into a directory of the zips name
+
+#For each zip, Check music library for directory of the album name. Return bool
+
+#***IF FALSE***
+
+#Move new directory in Artist directory
+
+#iterate over files to strip Artist + Album name from file name. LEAVE TRACK NUMBER
+
+#delete zip file in downloads folder
+
+#Return "Directory created."
+#Ask to open location
+#*IF YES* Open location and exit program
+#*IF NO* Exit program 
+
+#***IF TRUE***
+
+#Return "Album already exists."
+
+#Ask to if files should be deleted?
+
+#*IF YES* delete new directory with files
+
+#*IF NO* Exit program
