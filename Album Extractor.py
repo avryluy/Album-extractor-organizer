cd874001f93f90354638b27fa7eb9f63a8ba283c
Avry Luy <avryluy@mailbox.org>
2023-01-20 21:29:04 -0600

    added more variables to store paths and artist + album names. Created zipManager() function which extracts the zip into a folder, and checks the music library to see if the artist or album folder exists. It populates based on if then statements. Adjust sudo-code comments to live above relevant code for easier reading.

--- Album Extractor.py
+++ Album Extractor.py
@@ -2,18 +2,32 @@ import time as time
 import pathlib as path
 import shutil as sht
 
+
 #Declare Downloads path
-downloadFolder = path.Path(r"C:\Users\avryl\Downloads\AlbumExtractor")
+downloadPath = path.Path(r"C:\Users\avryl\Downloads\AlbumExtractor")
 #Declare music library path
-musicLibrary = ""
+musicLibrary = path.Path(r"C:\Users\avryl\Music")
+downloadFolder = str(downloadPath)
+#Store zip type in a variable
+zipSuffix = ".zip"
+
+def pathStatSize(x):
+    pathSize = x.stat().st_size
+    return pathSize 
+
 #Check for zip files. Determine how many there are.
 def zipCounter():
     count = 0
-    for child in downloadFolder.iterdir():
-            childstype = child.suffix
-            if childstype == ".zip":
-                count += 1
-            else: count += 0
+    for child in downloadPath.iterdir():
+        childstype = child.suffix
+        if childstype == ".zip":
+            count += 1
+        else: count += 0
+        #splice zip name to get Artist
+        print(child.name.split(".")[0])
+        #store zip path in a variable
+        childPath = downloadPath / child
+        # print(childPath)
     if  count == 1:
         print("There is 1 zip to be unpacked.")
     elif count == 0:
@@ -21,24 +35,55 @@ def zipCounter():
     else:
         print("There are {} zips to be unpacked.".format(count))
 
+
+def zipManager():
+    for child in downloadPath.iterdir():
+        if child.match("*.zip") == True:
+            #store zip path in a variable
+            childPath = downloadPath / child
+            print(childPath)
+            #Unpack the zipfile into a directory of the zips name
+            sht.unpack_archive(childPath,downloadPath,"zip")
+            print("Album Extracted")
+            folderName = child.name.split(".")[0]
+            #splice zip name to get Artist, store in variable
+            artistName = folderName.split(" - ")[0]
+            #splice zip name to get album name, store in variable
+            albumName = folderName.split(" - ")[1]
+            #capture path of extracted folder
+            extractFolder = downloadPath / folderName
+            print("******")
+            #Capture Music Library Artist Path
+            artistPath =  musicLibrary / artistName
+            #Check if artist folder exists
+            artistCheck = artistPath.exists()
+            if artistCheck == False:
+                print("Artist directory doesn't exist. Creating...")
+                #Create Artist folder if it doesn't exist
+                artistPath.mkdir()
+                print("Done.")
+            #Capture Music Library Album Path
+            albumPath = artistPath / albumName
+            #check if album exists
+            albumCheck = albumPath.exists()
+            #For each zip, Check music library for directory of the album name. Return bool
+            if albumCheck == False:
+                print("Moving files...")
+                #Move new directory in Artist directory
+                sht.move(extractFolder, albumPath)
+                print("Done.")
+            #If album folder exists but is empty, populate it with files.
+            elif albumCheck == True and (pathStatSize(albumPath) == 8192 or pathStatSize(albumPath) == 0):
+                print("Folder exists but nothing is inside. Copying...")
+                sht.copytree(extractFolder, albumPath, dirs_exist_ok=True)
+                print("Done.")
+            else:
+                #Return "Album already exists."
+                print("Album already exists here.")
+                
 zipCounter()
-
-#Store zip type in a variable
-
-#store zip name in a variable
-
-#splice zip name to get Artist, store in variable
-
-#splice zip name to get album name, store in variable
-
-#For each zip, Unpack the zipfile into a directory of the zips name
-###path.mkdir
-
-#For each zip, Check music library for directory of the album name. Return bool
-
-#***IF FALSE***
-
-#Move new directory in Artist directory
+print("******")
+zipManager()
 
 #iterate over files to strip Artist + Album name from file name. LEAVE TRACK NUMBER
 
@@ -51,7 +96,7 @@ zipCounter()
 
 #***IF TRUE***
 
-#Return "Album already exists."
+
 
 #Ask to if files should be deleted?
 
